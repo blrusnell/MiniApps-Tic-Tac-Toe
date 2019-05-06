aecdcfea3838ec27737743f338e15704fc706615
Brent Rusnell <blrusnell@gmail.com>
2019-05-06 15:26:20 -0500

    wrote out rows, cols, and diags

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+//getting each cell in table
+
+const htmlCollection = document.getElementsByTagName('td');
+const eachSquare = Array.prototype.slice.call(htmlCollection);
+
+
+//bool to handle player turn
 let turnBoolean = false;
 
+//puts x or o in cell
 const clickHandler = (e) => {
     if (e.target.innerHTML === "") {
         turnBoolean = !turnBoolean;
@@ -9,14 +17,24 @@ const clickHandler = (e) => {
             e.target.innerHTML = "O";
         }
     }
-}
-
-
-const htmlCollection = document.getElementsByTagName('td');
-
-const eachSquare = Array.prototype.slice.call( htmlCollection )
-
-
+};
+//apply click to each cell
 eachSquare.map(e => e.addEventListener("click", clickHandler));
 
+//array for each row, column, diagonal
+const row1 = document.getElementById("row1");
+const row2 = document.getElementById("row2");
+const row3 = document.getElementById("row3");
+const col1 = [row1[0], row2[0], row3[0]];
+const col2 = [row1[1], row2[1], row3[1]];
+const col3 = [row1[2], row2[2], row3[2]];
+const topRightFirstDiagonal = [row1[2], row2[1], row3[0]];
+const bottomRightFirstDiagonal = [row1[0], row2[1], row3[2]];
+
+    
+
+//checks for winners
+const winnerCheck = () => {
+    
+};
 
