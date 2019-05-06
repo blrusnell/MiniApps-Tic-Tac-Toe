00a80849dd3a1b38ae6f874bb01f00eeebc29647
Brent Rusnell <blrusnell@gmail.com>
2019-05-06 17:29:10 -0500

    minimum requirements done

--- app.js
+++ app.js
@@ -13,28 +13,68 @@ const clickHandler = (e) => {
         turnBoolean = !turnBoolean;
         if (turnBoolean) {
             e.target.innerHTML = "X";
+            winnerCheck();
         } else {
             e.target.innerHTML = "O";
+            winnerCheck();
         }
     }
 };
+
+
+
 //apply click to each cell
 eachSquare.map(e => e.addEventListener("click", clickHandler));
 
-//array for each row, column, diagonal
-const row1 = document.getElementById("row1");
-const row2 = document.getElementById("row2");
-const row3 = document.getElementById("row3");
-const col1 = [row1[0], row2[0], row3[0]];
-const col2 = [row1[1], row2[1], row3[1]];
-const col3 = [row1[2], row2[2], row3[2]];
-const topRightFirstDiagonal = [row1[2], row2[1], row3[0]];
-const bottomRightFirstDiagonal = [row1[0], row2[1], row3[2]];
 
-    
+
 
 //checks for winners
 const winnerCheck = () => {
-    
+    const one = document.getElementById('one').textContent;
+    const two = document.getElementById('two').textContent;
+    const three = document.getElementById('three').textContent;
+    const four = document.getElementById('four').textContent;
+    const five = document.getElementById('five').textContent;
+    const six = document.getElementById('six').textContent;
+    const seven = document.getElementById('seven').textContent;
+    const eight = document.getElementById('eight').textContent;
+    const nine = document.getElementById('nine').textContent;
+
+   let row1 = one + two + three;
+   let row2 = four + five + six;
+   let row3 = six + seven + eight;
+   let col1 = one + four + seven;
+   let col2 = two + five + eight;
+   let col3 = three + six + nine;
+   let diag1 = one + five + nine;
+   let diag2 = seven + five + three;
+
+   console.log(row1);
+   if (row1 === "XXX" || row1 === "OOO") {
+       alert('winner');
+   }
+   if (row2 === "XXX" || row2 === "OOO") {
+    alert('winner')
+   }
+   if (row3 === "XXX" || row3 === "OOO") {
+    alert('winner')
+   }
+   if (col1 === "XXX" || col1 === "OOO") {
+    alert('winner')
+   }
+   if (col2 === "XXX" || col2 === "OOO") {
+    alert('winner')
+   }
+   if (col3 === "XXX" || col3 === "OOO") {
+    alert('winner')
+   }
+   if (diag1 === "XXX" || diag1 === "OOO") {
+    alert('winner')
+   }
+   if (diag2 === "XXX" || diag2 === "OOO") {
+    alert('winner')
+   }
+
 };
 
